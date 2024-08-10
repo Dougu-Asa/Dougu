bda2c83e0d0426baa4b43d270528743ff7ef8a5e
Kane Li <kal036@ucsd.edu>
2024-08-09 20:31:02 -0700

    Equipment Overlay Animations

--- src/components/member/EquipmentOverlay.tsx
+++ src/components/member/EquipmentOverlay.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Overlay, Card, Button } from "@rneui/themed";
-import { Text, StyleSheet, Pressable, View } from "react-native";
+import { Card, Button } from "@rneui/themed";
+import { Text, StyleSheet, Pressable } from "react-native";
 import { LinearGradient } from "expo-linear-gradient"; // Import the LinearGradient component
+import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
 
 import { useEquipment } from "../../helper/context/EquipmentContext";
 import { ScrollView } from "react-native-gesture-handler";
@@ -16,46 +17,49 @@ export default function EquipmentOverlay() {
     useEquipment();
 
   return (
-    <Overlay isVisible={visible} fullScreen={true}>
-      <Text style={styles.title}>{equipmentItem?.label}</Text>
-      <View style={styles.infoContainer}>
-        <Text style={styles.infoTxt}>
-          The outlined equipment is the one which gets swapped!
-        </Text>
-      </View>
-      <ScrollView>
-        {equipmentItem &&
-          equipmentItem.data.map((itemId, index) => (
-            <Pressable
-              key={index}
-              onPress={() => {
-                modifyEquipmentItem(equipmentItem, itemId);
-              }}
-            >
-              <Card
-                containerStyle={
-                  equipmentItem.id === itemId && styles.highlighted
-                }
-              >
-                <Card.Title>{itemId}</Card.Title>
-                <Card.Divider />
-                <Text>{equipmentItem.detailData[index]}</Text>
-              </Card>
-            </Pressable>
-          ))}
-      </ScrollView>
-      <Button
-        ViewComponent={LinearGradient}
-        linearGradientProps={{
-          colors: ["#FF9800", "#F44336"],
-          start: { x: 0, y: 0.5 },
-          end: { x: 1, y: 0.5 },
-        }}
-        onPress={() => setVisible(false)}
-      >
-        Close
-      </Button>
-    </Overlay>
+    <>
+      {visible && (
+        <Animated.View
+          style={styles.overlayStyles}
+          entering={FadeIn}
+          exiting={FadeOut}
+        >
+          <Text style={styles.title}>{equipmentItem?.label}</Text>
+          <ScrollView>
+            {equipmentItem &&
+              equipmentItem.data.map((itemId, index) => (
+                <Pressable
+                  key={index}
+                  onPress={() => {
+                    modifyEquipmentItem(equipmentItem, itemId);
+                  }}
+                >
+                  <Card
+                    containerStyle={
+                      equipmentItem.id === itemId && styles.highlighted
+                    }
+                  >
+                    <Card.Title>{itemId}</Card.Title>
+                    <Card.Divider />
+                    <Text>{equipmentItem.detailData[index]}</Text>
+                  </Card>
+                </Pressable>
+              ))}
+          </ScrollView>
+          <Button
+            ViewComponent={LinearGradient}
+            linearGradientProps={{
+              colors: ["#FF9800", "#F44336"],
+              start: { x: 0, y: 0.5 },
+              end: { x: 1, y: 0.5 },
+            }}
+            onPress={() => setVisible(false)}
+          >
+            Close
+          </Button>
+        </Animated.View>
+      )}
+    </>
   );
 }
 
@@ -78,9 +82,11 @@ const styles = StyleSheet.create({
     marginLeft: 20,
   },
   overlayStyles: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
+    backgroundColor: "white",
+    flex: 1,
+    position: "absolute",
+    width: "100%",
+    height: "100%",
   },
   title: {
     marginTop: 20,
