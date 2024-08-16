afe2e7edb02956838fb46f264e582ec8239b86de
Kane Li <kal036@ucsd.edu>
2024-08-16 15:51:00 -0700

    create spreadsheet
    create and share a spreadsheet with the owner using a lambda function

--- lambda/nodejs/index.js
+++ lambda/nodejs/index.js
@@ -1,62 +1,58 @@
+const { GoogleAuth } = require("google-auth-library");
+const { google } = require("googleapis");
+
+const auth = new GoogleAuth({
+  scopes: "https://www.googleapis.com/auth/drive",
+});
+const driveService = google.drive({ version: "v3", auth });
+const sheetService = google.sheets({ version: "v4", auth });
+
 /**
  * Create a google spreadsheet
  * @param {string} title Spreadsheets title
  * @return {string} Created spreadsheets ID
  */
 async function create(title) {
-  const { GoogleAuth } = require("google-auth-library");
-  const { google } = require("googleapis");
-
-  const auth = new GoogleAuth({
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
-
-  const service = google.sheets({ version: "v4", auth });
   const resource = {
     properties: {
       title,
     },
   };
   try {
-    const spreadsheet = await service.spreadsheets.create({
+    const spreadsheet = await sheetService.spreadsheets.create({
       resource,
       fields: "spreadsheetId",
     });
     console.log(`Spreadsheet ID: ${spreadsheet.data.spreadsheetId}`);
     return spreadsheet.data.spreadsheetId;
   } catch (err) {
-    // TODO (developer) - Handle exception
+    console.log("create error: ", err);
     throw err;
   }
 }
 
 async function share(spreadsheetId, email) {
-  const { GoogleAuth } = require("google-auth-library");
-  const { google } = require("googleapis");
-
-  const auth = new GoogleAuth({
-    scopes: "https://www.googleapis.com/auth/drive",
-  });
-
-  const service = google.drive({ version: "v3", auth });
+  console.log("credentialsl oaded");
   const resource = {
     type: "user",
     role: "writer",
     emailAddress: email,
   };
   try {
-    await service.permissions.create({
+    await driveService.permissions.create({
       resource,
       fileId: spreadsheetId,
       fields: "id",
     });
     console.log(`Spreadsheet shared with: ${email}`);
   } catch (err) {
-    // TODO (developer) - Handle exception
+    console.log("share error: ", err);
     throw err;
   }
 }
 
-//create("My Spreadsheet");
-share("1KQqd4clySh9AKH9ZWioIXn0O5MXYWd5E-X6Vux2iPhU", "kal036@ucsd.edu");
-console.log("Function executed");
+exports.handler = async (event) => {
+  const { title, email } = event;
+  const spreadsheetId = await create(title);
+  await share(spreadsheetId, email);
+};
